c95866adb3de3213262535e10c66d49914381348
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-04-07 19:53:29 -0300

    feat: Colocando as pastas de database a auth

---
+++ mobile/app/src/main/java/br/edu/puc/superid/database/FirestoreHandler.kt
@@ -0,0 +1,10 @@
+package br.edu.puc.superid.database
+
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class FirestoreHandler {
+    val db = Firebase.firestore
+
+
+}
