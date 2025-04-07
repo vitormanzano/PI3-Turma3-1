9332a72b3ba39d9c1b3272c8c0555cd5c4c0bd2a
Guilherme Giusepe Piovezan <161312276+gpiovezan@users.noreply.github.com>
2025-04-07 17:00:40 -0300

    style: melhora no layout e design
    Alterações no design e layout seguindo o figma

--- mobile/app/src/main/java/br/edu/puc/superid/MainActivity.kt
+++ mobile/app/src/main/java/br/edu/puc/superid/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import br.edu.puc.superid.ui.screens.LoginScreen
 import br.edu.puc.superid.ui.theme.SuperIdTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperIdTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LoginScreen(
+                    onLoginSuccess = {
+                        // TO DO navegar para outra tela quando o login for concluido com sucesso
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SuperIdTheme {
-        Greeting("Android")
     }
 }
--- mobile/app/src/main/java/br/edu/puc/superid/ui/screens/LoginScreen.kt
+++ mobile/app/src/main/java/br/edu/puc/superid/ui/screens/LoginScreen.kt
@@ -13,37 +13,129 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material3.*
+import androidx.compose.ui.*
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 
+
+// TO DO fazer a parte de login utilizando o auth
 @Composable
-fun LoginScreen(onLoginSuccess: () -> Unit) {
+fun LoginScreen(onLoginSuccess: () -> Unit = {}) {
     var email by remember { mutableStateOf("") }
     var senha by remember { mutableStateOf("") }
 
-    Column {
-        Text("Login", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+    val backgroundColor = Color(0xFF102952) // fundo azul escuro
+    val buttonColor = Color(0xFF00D7FF)
 
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("E-mail") }
-        )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(32.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Lock,
+                    contentDescription = "Lock Icon",
+                    tint = Color.White,
+                    modifier = Modifier.size(55.dp)
+                )
 
-        OutlinedTextField(
-            value = senha,
-            onValueChange = { senha = it },
-            label = { Text("Senha") },
-        )
+                Spacer(modifier = Modifier.width(4.dp))
 
-        Button(onClick = onLoginSuccess) {
-            Text("Entrar")
+                Text(
+                    text = "Super ID",
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+            }
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text(
+                    color = Color.Gray,
+                    text = "E-mail") },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Outlined.Email, contentDescription = null, tint = buttonColor)
+                },
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(52.dp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = buttonColor,
+                    unfocusedBorderColor = Color.DarkGray,
+                    cursorColor = buttonColor
+                )
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = senha,
+                onValueChange = { senha = it },
+                label = { Text(
+                    color = Color.Gray,
+                    text="Senha mestra") },
+                visualTransformation = PasswordVisualTransformation(),
+                leadingIcon = {
+                    Icon(imageVector = Icons.Outlined.Lock, contentDescription = null, tint = buttonColor)
+                },
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(52.dp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = buttonColor,
+                    unfocusedBorderColor = Color.DarkGray,
+                    cursorColor = buttonColor
+                )
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = onLoginSuccess,
+                colors = ButtonDefaults.buttonColors(containerColor = buttonColor),
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(52.dp),
+            ) {
+                Text("ENTRAR", fontWeight = FontWeight.Bold, color = backgroundColor)
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(
+                text = "Ainda não possui uma conta?",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color.White
+            )
         }
-        Text(
-            text = "Ainda não possui uma conta?",
-            fontSize = 14.sp,
-            color = Color.Gray
-        )
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
