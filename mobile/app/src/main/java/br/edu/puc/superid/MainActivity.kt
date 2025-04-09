6b783e522cdd21d192ea9b61df808ac51efbadc2
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-04-09 16:54:07 -0300

    Cadastrar credencial e cadastrar no firestore

--- mobile/app/src/main/java/br/edu/puc/superid/MainActivity.kt
+++ mobile/app/src/main/java/br/edu/puc/superid/MainActivity.kt
@@ -1,11 +1,13 @@
 package br.edu.puc.superid
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import br.edu.puc.superid.ui.screens.LoginScreen
 import br.edu.puc.superid.ui.theme.SuperIdTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +18,21 @@ class MainActivity : ComponentActivity() {
                 LoginScreen(
                     onLoginSuccess = {
                         // TO DO navegar para outra tela quando o login for concluido com sucesso
+//                        val PREFS_NAME = "MyPrefsFile"
+//
+//                        val settings = getSharedPreferences(PREFS_NAME, 0)
+//
+//                        if (settings.getBoolean("my_first_time", true)) {
+//                            //the app is being launched for first time, do something
+//                            Log.d("Comments", "First time")
+//
+//                            // first time task
+//
+//                            // record the fact that the app has been started at least once
+//                            settings.edit().putBoolean("my_first_time", false).commit()
+//                        } -> Verifica se é a primeira vez que ele entra no app
                     }
+
                 )
             }
         }
--- mobile/app/src/main/java/br/edu/puc/superid/database/FirestoreHandler.kt
+++ mobile/app/src/main/java/br/edu/puc/superid/database/FirestoreHandler.kt
@@ -1,10 +1,27 @@
 package br.edu.puc.superid.database
 
+import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class FirestoreHandler {
-    val db = Firebase.firestore
+    private val db = Firebase.firestore
+    //Falta colocar IMEI aqui
+    fun cadastrarUsuario(nome: String, uid: String) {
+        val novoDocUsuario = hashMapOf(
+            "Nome" to nome,
+            "UID" to uid
+        )
 
-
+        db.collection("users").add(novoDocUsuario)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("SUCCESS", "Usuário criado!")
+                }
+                else {
+                    Log.w("FAILURE", "${task.exception}")
+                }
+            }
+    }
 }
+
