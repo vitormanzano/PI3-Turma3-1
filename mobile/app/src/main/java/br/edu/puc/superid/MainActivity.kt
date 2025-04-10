8541776861f442903d28e02ec2a6c898d067c3b8
Vitor Manzano <vimanzanovillela@gmail.com>
2025-04-10 17:22:03 -0300

    Primeira vez entrando no app funcionando, falta refatorar

--- mobile/app/src/main/java/br/edu/puc/superid/MainActivity.kt
+++ mobile/app/src/main/java/br/edu/puc/superid/MainActivity.kt
@@ -20,7 +20,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            
+            val PREFS_NAME = "MyPrefsFile"
+
+            val settings = getSharedPreferences(PREFS_NAME, 0)
+
+            if (settings.getBoolean("my_first_time", true)) {
+                //the app is being launched for first time, do something
+
+                FirstTimeScreen()
+
+                // first time task
+
+                // record the fact that the app has been started at least once
+                settings.edit().putBoolean("my_first_time", false).commit()
+            }
+            else {
+                val androidId = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+                SignUpScreen(androidId)
+            }
         }
     }
 }
