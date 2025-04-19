95fa4260558a683164d9bb15fcd71298f2cbdb13
Vitor Manzano <vimanzanovillela@gmail.com>
2025-04-19 14:59:49 -0300

    Criar senha sem criptografia e teste de criptografia

--- mobile/app/src/main/java/br/edu/puc/superid/database/FirestoreHandler.kt
+++ mobile/app/src/main/java/br/edu/puc/superid/database/FirestoreHandler.kt
@@ -3,6 +3,9 @@ package br.edu.puc.superid.database
 import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import android.util.Base64
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
 
 class FirestoreHandler {
     private val db = Firebase.firestore
@@ -18,11 +21,63 @@ class FirestoreHandler {
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     Log.d("SUCCESS", "Usuário criado!")
+
                 }
                 else {
                     Log.w("FAILURE", "${task.exception}")
                 }
             }
     }
+
+    fun cadastrarSenha(uid: String, login: String?, categoria: String, senha: String) {
+        val docId = db.collection("users")
+            .whereEqualTo("UID", uid)
+            .get()
+            .addOnSuccessListener { documents ->
+                if (!documents.isEmpty) {
+                    val document = documents.documents[0]
+                    val userDocRef = document.reference
+
+                    // Agora criamos a subcoleção "senhas"
+                    val senhaData = hashMapOf(
+                        "login" to login,
+                        "categoria" to categoria,
+                        "senha" to senha
+                    )
+
+                    userDocRef.collection("senhas")
+                        .add(senhaData)
+                        .addOnSuccessListener {
+                            println("Senha cadastrada com sucesso!")
+                        }
+                        .addOnFailureListener { e ->
+                            println("Erro ao cadastrar senha: $e")
+                        }
+                } else {
+                    println("Usuário com UID $uid não encontrado.")
+                }
+            }
+    }
+
+    fun encrypt(senha: String, chave: String): String {
+        val keyBytes = chave.toByteArray(Charsets.UTF_8)
+        val secretKey = SecretKeySpec(keyBytes.copyOf(16), "AES")
+
+        val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+
+        val encryptedBytes = cipher.doFinal(senha.toByteArray(Charsets.UTF_8))
+        return Base64.encodeToString(encryptedBytes, Base64.DEFAULT)
+    }
+
+    fun criptografarSenha(senha: String): String {
+        val chaveParaCriptografar = "chaveExemplo123"
+
+        val senhaCriptografada = encrypt(senha, chaveParaCriptografar)
+
+        return senhaCriptografada
+    }
 }
+
+
 
