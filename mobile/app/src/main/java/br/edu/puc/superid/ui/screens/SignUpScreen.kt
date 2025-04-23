6854aca21396d0faf30e099a2c2eea56020fb5aa
Guilherme Giusepe Piovezan <161312276+gpiovezan@users.noreply.github.com>
2025-04-23 14:57:16 -0300

    feedback de criacao de conta

--- mobile/app/src/main/java/br/edu/puc/superid/ui/screens/SignUpScreen.kt
+++ mobile/app/src/main/java/br/edu/puc/superid/ui/screens/SignUpScreen.kt
@@ -1,20 +1,6 @@
 package br.edu.puc.superid.ui.screens
 
-import android.provider.Settings
 import android.util.Log
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -26,18 +12,24 @@ import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material3.*
-import androidx.compose.ui.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import br.edu.puc.superid.auth.AuthHandler
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-
-// TO DO fazer a parte de login utilizando o auth
 @Composable
 fun SignUpScreen(imei: String, navController: NavHostController) {
     var name by remember { mutableStateOf("")}
@@ -47,193 +39,201 @@ fun SignUpScreen(imei: String, navController: NavHostController) {
     val auth = AuthHandler()
     var showDialog by remember { mutableStateOf(false) }
 
-    val backgroundColor = Color(0xFF102952) // fundo azul escuro
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+    val backgroundColor = Color(0xFF102952)
     val iconsColor = Color(0xFF00D7FF)
     val buttonColor = Color(0xFF00D7FF)
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(backgroundColor),
-        contentAlignment = Alignment.Center
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
+    Scaffold(
+        containerColor = backgroundColor,
+        snackbarHost = { SnackbarHost(snackbarHostState) }
+    ) { innerPadding ->
+        Box(
             modifier = Modifier
-                .padding(32.dp)
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(32.dp)
             ) {
-                Icon(
-                    imageVector = Icons.Filled.Lock,
-                    contentDescription = "Lock Icon",
-                    tint = Color.White,
-                    modifier = Modifier.size(55.dp)
-                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Lock,
+                        contentDescription = "Lock Icon",
+                        tint = Color.White,
+                        modifier = Modifier.size(55.dp)
+                    )
 
-                Spacer(modifier = Modifier.width(4.dp))
+                    Spacer(modifier = Modifier.width(4.dp))
 
-                Text(
-                    text = "Super ID",
-                    fontSize = 48.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White
-                )
-            }
+                    Text(
+                        text = "Super ID",
+                        fontSize = 48.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                }
 
-            Spacer(modifier = Modifier.height(40.dp))
+                Spacer(modifier = Modifier.height(40.dp))
 
-            OutlinedTextField(
-                value = name,
-                onValueChange = { name = it },
-                placeholder = { Text(
-                    color = Color.Gray,
-                    text = "Nome completo") },
-                leadingIcon = {
-                    Icon(imageVector = Icons.Outlined.AccountCircle, contentDescription = null, tint = iconsColor)
-                },
-                textStyle = TextStyle(color = Color.White),
-                shape = RoundedCornerShape(10.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(52.dp),
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = iconsColor,
-                    unfocusedBorderColor = Color.DarkGray,
-                    cursorColor = iconsColor
-                )
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            OutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                placeholder = { Text(
-                    color = Color.Gray,
-                    text = "E-mail") },
-                leadingIcon = {
-                    Icon(imageVector = Icons.Outlined.Email, contentDescription = null, tint = iconsColor)
-                },
-                textStyle = TextStyle(color = Color.White),
-                shape = RoundedCornerShape(10.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(52.dp),
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = iconsColor,
-                    unfocusedBorderColor = Color.DarkGray,
-                    cursorColor = iconsColor
-                )
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            OutlinedTextField(
-                value = senha,
-                onValueChange = { senha = it },
-                placeholder = { Text(
-                    color = Color.Gray,
-                    text="Senha mestra") },
-                visualTransformation = PasswordVisualTransformation(),
-                leadingIcon = {
-                    Icon(imageVector = Icons.Outlined.Lock, contentDescription = null, tint = iconsColor)
-                },
-                textStyle = TextStyle(color = Color.White),
-                shape = RoundedCornerShape(10.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(52.dp),
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedBorderColor = iconsColor,
-                    unfocusedBorderColor = Color.DarkGray,
-                    cursorColor = iconsColor
-                )
-            )
-
-            Spacer(modifier = Modifier.width(8.dp))
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(top = 16.dp),
-            ) {
-                Checkbox(
-                    checked = checked,
-                    onCheckedChange = { checked = it },
-                    colors = CheckboxDefaults.colors(
-                        checkedColor = iconsColor,
-                        uncheckedColor = Color.Gray,
-                        checkmarkColor = Color.White
+                OutlinedTextField(
+                    value = name,
+                    onValueChange = { name = it },
+                    placeholder = { Text("Nome completo", color = Color.Gray) },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Outlined.AccountCircle, contentDescription = null, tint = iconsColor)
+                    },
+                    textStyle = TextStyle(color = Color.White),
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(52.dp),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = iconsColor,
+                        unfocusedBorderColor = Color.DarkGray,
+                        cursorColor = iconsColor
                     )
                 )
-                Spacer(modifier = Modifier.width(8.dp))
-                val annotatedText = buildAnnotatedString {
-                    append("Li e estou de acordo com o ")
-                    pushStringAnnotation(tag = "TERMOS", annotation = "mostrar_dialog")
-                    withStyle(style = SpanStyle(color = iconsColor, fontWeight = FontWeight.Bold)) {
-                        append("Termo de Uso")
-                    }
-                    pop()
-                }
 
-                ClickableText(
-                    text = annotatedText,
-                    onClick = { offset ->
-                        annotatedText.getStringAnnotations(tag = "TERMOS", start = offset, end = offset)
-                            .firstOrNull()?.let {
-                                showDialog = true
-                            }
+                Spacer(modifier = Modifier.height(16.dp))
+
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    placeholder = { Text("E-mail", color = Color.Gray) },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Outlined.Email, contentDescription = null, tint = iconsColor)
                     },
-                    style = TextStyle(color = Color.White)
+                    textStyle = TextStyle(color = Color.White),
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(52.dp),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = iconsColor,
+                        unfocusedBorderColor = Color.DarkGray,
+                        cursorColor = iconsColor
+                    )
                 )
 
-            }
+                Spacer(modifier = Modifier.height(16.dp))
 
-            Spacer(modifier = Modifier.height(24.dp))
+                OutlinedTextField(
+                    value = senha,
+                    onValueChange = { senha = it },
+                    placeholder = { Text("Senha mestra", color = Color.Gray) },
+                    visualTransformation = PasswordVisualTransformation(),
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Outlined.Lock, contentDescription = null, tint = iconsColor)
+                    },
+                    textStyle = TextStyle(color = Color.White),
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(52.dp),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = iconsColor,
+                        unfocusedBorderColor = Color.DarkGray,
+                        cursorColor = iconsColor
+                    )
+                )
 
-            Button(
-                onClick = {
-                    auth.cadastrarCredencial(name, email, senha, imei)
-                },
-                colors = ButtonDefaults.buttonColors(containerColor = buttonColor),
-                shape = RoundedCornerShape(10.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(52.dp),
-            ) {
-                Text("CRIAR CONTA", fontWeight = FontWeight.Bold, color = backgroundColor)
-            }
+                Spacer(modifier = Modifier.width(8.dp))
 
-            Spacer(modifier = Modifier.height(24.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(top = 16.dp),
+                ) {
+                    Checkbox(
+                        checked = checked,
+                        onCheckedChange = { checked = it },
+                        colors = CheckboxDefaults.colors(
+                            checkedColor = iconsColor,
+                            uncheckedColor = Color.Gray,
+                            checkmarkColor = Color.White
+                        )
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
 
-            Text(
-                text = "Já possui uma conta?",
-                fontSize = 16.sp,
-                fontWeight = FontWeight.SemiBold,
-                color = Color.White,
-                modifier = Modifier.clickable {
-                    navController.navigate("login")
+                    val annotatedText = buildAnnotatedString {
+                        append("Li e estou de acordo com o ")
+                        pushStringAnnotation(tag = "TERMOS", annotation = "mostrar_dialog")
+                        withStyle(style = SpanStyle(color = iconsColor, fontWeight = FontWeight.Bold)) {
+                            append("Termo de Uso")
+                        }
+                        pop()
+                    }
+
+                    ClickableText(
+                        text = annotatedText,
+                        onClick = { offset ->
+                            annotatedText.getStringAnnotations(tag = "TERMOS", start = offset, end = offset)
+                                .firstOrNull()?.let {
+                                    showDialog = true
+                                }
+                        },
+                        style = TextStyle(color = Color.White)
+                    )
                 }
-            )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Button(
+                    onClick = {
+                        auth.cadastrarCredencial(name, email, senha, imei)
+                        scope.launch {
+                            snackbarHostState.showSnackbar("Conta criada com sucesso!")
+                            delay(2000)
+                            navController.navigate("login")
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(containerColor = buttonColor),
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(52.dp),
+                ) {
+                    Text("CRIAR CONTA", fontWeight = FontWeight.Bold, color = backgroundColor)
+                }
+
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Text(
+                    text = "Já possui uma conta?",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.White,
+                    modifier = Modifier.clickable {
+                        navController.navigate("login")
+                    }
+                )
+            }
+
+            if (showDialog) {
+                AlertDialog(
+                    onDismissRequest = { showDialog = false },
+                    confirmButton = {
+                        TextButton(onClick = { showDialog = false }) {
+                            Text("Fechar")
+                        }
+                    },
+                    title = {
+                        Text("Termos de Uso")
+                    },
+                    text = {
+                        Text("termos.")
+                    },
+                    containerColor = Color.White
+                )
+            }
         }
     }
-    if (showDialog) {
-        AlertDialog(
-            onDismissRequest = { showDialog = false },
-            confirmButton = {
-                TextButton(onClick = { showDialog = false }) {
-                    Text("Fechar")
-                }
-            },
-            title = {
-                Text("Termos de Uso")
-            },
-            text = {
-                Text("termos.")
-            },
-            containerColor = Color.White
-        )
-    }
-
 }
